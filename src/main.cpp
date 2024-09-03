e9c43846d30509ed4b5946a415dbcfc206eee609
DESKTOP-T3JHKNK\alex <574817827@qq.com>
2024-09-03 21:18:36 +0200

    comments in Eng

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include <mpi.h>  // 包含 MPI 库头文件
+#include <mpi.h>  // Includes MPI library header files
 #include <iostream>
 
 int main(int argc, char** argv) {
-    // 初始化 MPI 环境
+    // Initialize MPI environment
     MPI_Init(&argc, &argv);
 
-    // 获取进程总数和当前进程的 ID
+    // Get the total number of processes and the ID of the current process
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
@@ -18,19 +18,19 @@ int main(int argc, char** argv) {
     }
 
     if (world_rank == 0) {
-        // 进程 0 发送一个整数到进程 1
+        // Process 0 sends an integer to process 1
         int number = 42;
         std::cout << "Process " << world_rank << " sending number " << number << " to process 1" << std::endl;
         MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
         std::cout << "Process " << world_rank << " sent number " << number << " to process 1" << std::endl;
     } else if (world_rank == 1) {
-        // 进程 1 接收来自进程 0 的整数
+        // Process 1 receives the integer from process 0
         int number;
         MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         std::cout << "Process " << world_rank << " received number " << number << " from process 0" << std::endl;
     }
 
-    // 结束 MPI 环境
+    // End MPI environment
     MPI_Finalize();
 
     return 0;
